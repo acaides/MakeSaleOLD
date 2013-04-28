7e64360df70701384cbab231fc020cc693a1393e
Isaiah Simpson <isaiah.simpson@gmail.com>
2013-04-28 18:11:59 -0500

    fix project setup and improve configuration

--- backends/api/api.js
+++ backends/api/api.js
@@ -1,38 +1,36 @@
-var config = require('config'),	// npm install config
-	mysql = require('mysql'),	// npm install mysql@2.0.0-alpha7
-	dbConnection = mysql.createConnection({
-		host: config.database.host,
-		user: config.database.user,
-		password: config.database.password
-	}),
-	Memcached = require('memcached'),	// npm install memcached
-	memcached = new Memcached(config.cache.serverLocations, config.cache.options),
-	solr = require('solr-client'),	// npm install solr-client
-	solrClient = solr.createClient(config.search.clientOptions),
-	express = require('express'),	// npm install -g express
-	swagger = require('swagger-node-express'),
+var config = require('config'),
+	apiConfig = config.api,
+	_ = require('lodash'),
+	express = require('express'),
 	api = express(),
-	betaApi = require('MSBetaApi.js');
-//	v1Api = require('MSVersionOneApi.js');
+	impls = [];
+	// Memcached = require('memcached'),	// npm install memcached
+	// memcached = new Memcached(config.cache.serverLocations, config.cache.options),
+	// solr = require('solr-client'),	// npm install solr-client
+	// solrClient = solr.createClient(config.search.clientOptions),
 
+// Setup some basic middleware used by all impls
 api.use(express.json());	// Parse request body JSON.
 api.use(express.compress());	// Compress response body JSON.
 api.use(express.query());	// Parse query string parameters.
-api.use(express.basicAuth());	// Parse HTTP Basic Auth header.
-api.use(require('MSAuth.js')());	// Process MakeSale authorization. 
 
-// Setup api versions
-betaApi(api);
-//v1Api(api);
+// this probably belongs in the impls
+//api.use(express.basicAuth());	// Parse HTTP Basic Auth header.
+//api.use(require('MSAuth.js')());	// Process MakeSale authorization. 
 
-
+// Require, register, and bind all impls.
+_.forEach(apiConfig.impls, function (implName) {
+	var impl = require(config[implName].module);
+	impls.push(impl);
+	impl.bind(api);
+});
 
 // Allowing all CORs, for now
-swagger.setHeaders = function setHeaders(res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, X-API-KEY');
-  res.header('Content-Type', 'application/json; charset=utf-8');
-};
+// swagger.setHeaders = function setHeaders(res) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, X-API-KEY');
+//   res.header('Content-Type', 'application/json; charset=utf-8');
+// };
 
-api.listen(3000);
+api.listen(api.portNumber);
--- backends/api/impls/beta/MSBetaApi.js
+++ backends/api/impls/beta/MSBetaApi.js
@@ -1,47 +1,52 @@
-module.exports = function MSBetaApiConstructor (api) {
-    var express = require('express'),
+exports.bind = function MSBetaApiBinder (api) {
+    var betaConfig = require('config').beta,
+        express = require('express'),
         swagger = require('swagger-node-express'),
         _ = require('lodash'),
 
         // Bind Sequelize to the MakeSale MySQL database.
-        $ = new require('sequelize-mysql').sequelize('MakeSale', 'username', ''),
+        $ = new require('sequelize-mysql').sequelize(
+            betaConfig.database.name,
+            betaConfig.database.user,
+            betaConfig.database.password
+        ),
 
         beta = express(),   // The beta API.
         $$ = {},    // The MakeSale data model. 
 
         // Resource controllers
-        MSUsersController = require('MSUsersController.js'),
-        MSMakersController = require('MSMakersController.js'),
-        MSProductsController = require('MSProductsController.js'),
+        // MSUsersController = require('./controllers/MSUsersController.js'),
+        // MSMakersController = require('MSMakersController.js'),
+        MSProductsController = require.main.require(betaConfig.controllers.MSProductsController),
 
         // Resource models
         resourceModel = {
-            User: require('MSUserResourceModel.json'),
-            Maker: require('MSMakerResourceModel.json'),
-            Product: require('MSProductResourceModel.json')
+            // User: require('MSUserResourceModel.json'),
+            // Maker: require('MSMakerResourceModel.json'),
+            Product: require.main.require(betaConfig.resourceModels.MSProductResourceModel)
         },
 
         // ORM 
-        MSUserORM = require('MSUserORM.js'),
-        MSMakerORM = require('MSMakerORM.js'),
-        MSProductORM = require('MSProductORM.js');
+        // MSUserORM = require('MSUserORM.js'),
+        // MSMakerORM = require('MSMakerORM.js'),
+        MSProductORM = require.main.require(betaConfig.orms.MSProductORM);
 
-    // Bind the models to the db and compose the data model. 
-    _.extend($$, MSUserORM.bind($));
-    _.extend($$, MSMakerORM.bind($));
-    _.extend($$, MSProductORM.bind($));
+    // // Bind the models to the db and compose the data model. 
+    // // _.extend($$, MSUserORM.bind($));
+    // // _.extend($$, MSMakerORM.bind($));
+    // _.extend($$, MSProductORM.bind($));
 
-    // Attach the beta API to the api service and setup swagger handling.
-    api.use('/beta', beta);
-    swagger.setAppHandler(beta);
+    // // Attach the beta API to the api service and setup swagger handling.
+    // api.use('/beta', beta);
+    // swagger.setAppHandler(beta);
 
-    // Bind resource controllers to the beta API and the data model.
-    MSUsersController.bind(swagger, $$);
-    MSMakersController.bind(swagger, $$);
-    MSProductsController.bind(swagger, $$);
+    // // Bind resource controllers to the beta API and the data model.
+    // // MSUsersController.bind(swagger, $$);
+    // // MSMakersController.bind(swagger, $$);
+    // MSProductsController.bind(swagger, $$);
 
-    swagger.configure('http://api.makesale.co', 'beta');
+    // swagger.configure('http://api.makesale.co', 'beta');
 
-    //swagger.addValidator(function MSExampleValidator (req, path, httpMethod) {});
-    swagger.addModels(resourceModel);
+    // //swagger.addValidator(function MSExampleValidator (req, path, httpMethod) {});
+    // swagger.addModels(resourceModel);
 };
